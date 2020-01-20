fde9d0ac80f023c1de3415b4f62b1af45e346aa5
Jesus Gtz <42782980+jgutierrez32@users.noreply.github.com>
2020-01-20 13:51:47 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="stylesheet-large-screens.css" />
+    <link rel="stylesheet" href="stylesheet-medium-screens.css" />
+    <link rel="stylesheet" href="stylesheet-small-screens.css" />
+    <link rel="stylesheet" href="stylesheet-tiny-screens.css" />
+    <link rel="stylesheet" href="stylesheet-tablet-screens.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Tajawal&display=swap"
+      rel="stylesheet"
+    />
+    <title>Financial Management | Smarter Graduate</title>
+  </head>
+
+  <!-- Nav Integration -->
+  <header>
+    <a href="#" class="site-logo" aria-label="homepage">SmarterGraduate</a>
+    <nav class="main-nav">
+      <ul class="nav__list">
+        <li class="nav__list-item">
+          <a href="#" class="nav__link">Home</a>
+        </li>
+        <li class="nav__list-item">
+          <a href="#" class="nav__link">College | Finances</a>
+        </li>
+        <li class="nav__list-item">
+          <a href="#" class="nav__link">How it works</a>
+        </li>
+        <li class="nav__list-item">
+          <a href="#" class="nav__link">Help</a>
+        </li>
+      </ul>
+    </nav>
+    <nav class="account">
+      <ul class="nav__list">
+        <li class="nav__list-item">
+          <a class="nav__link nav__link--btn nav__link--btn--highlight" href="#"
+            >Join</a
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- Nav Integration -->
+
+  <body>
+    <!-- Landing Section Page -->
+    <section class="home-intro">
+      <div class="intro-grid">
+        <div class="intro-info">
+          <h1>Smarter Graduate</h1>
+          <h2>Visualize Your Career Options</h2>
+
+          <a href="#" class="start-here-btn">Start Here</a>
+        </div>
+      </div>
+    </section>
+    <!-- Landing Section Page -->
+
+    <!--  Dashboard Section Page -->
+    <div class="dashboard-grid">
+      <div class="dashboard-intro">
+        <h1>Manage your personal dashboard.</h1>
+      </div>
+      <div class="dashboard-dashboard">
+        <img src="images/dashboard-image.png" alt="" />
+      </div>
+      <div class="dashboard-info">
+        <h1>
+          With your personalized dashboard we make it easier for you to see if
+          your investments are worth it.
+        </h1>
+        <hr6></hr6>
+        <h3>> Plan for your education.</h3>
+        <h3>> Browse previous and current scenarios.</h3>
+        <h3>> Plan for your retirement.</h3>
+        <h3>> Compare all your paths.</h3>
+        <h3>> Learn the benefits of college.</h3>
+        <a href="#" class="learn-more-btn">Learn More</a>
+      </div>
+    </div>
+    <!--  Dashboard Section Page-->
+
+    <!-- Financial Planning Section -->
+    <div class="financial-grid">
+      <div class="financial-info">
+        <h1>01</h1>
+        <h2>benefits of college</h2>
+        <p>
+          College is a stepping stone to achieving success. From medicine to the
+          arts, being a college graduate means you've made yourself more
+          competitive. <br />
+          <br />
+          Average lifetime earnings are almost 3x for professional school
+          graduates, and studies show that those with graduate degrees earn on
+          average $1.4 million more in median life earnings than high school
+          graduates.
+        </p>
+      </div>
+      <div class="financial-image">
+        <img src="images/benefits.png" alt="benefits of college" />
+      </div>
+    </div>
+    <!-- Financial Planning Section -->
+
+    <!-- Costs Section -->
+    <div class="costs-grid">
+      <div class="costs-image">
+        <img src="images/costs.png" alt="costs of college" />
+      </div>
+      <div class="costs-info">
+        <h1>02</h1>
+        <h2>The costs</h2>
+        <p>
+          A college degree is one of the biggest investments that you will make
+          in your lifetime. With any investment, you must understand what you
+          are paying into it, and receiving from it. <br />
+          <br />Knowing this gives you a true sense of what impact this
+          investment makes on your personal and professional life, from today
+          until the day you retire.
+        </p>
+        <a href="#" class="more-info-btn">More Info</a>
+      </div>
+    </div>
+    <!-- Costs Section -->
+
+    <!-- Future Section -->
+    <div class="future-grid">
+      <div class="future-info">
+        <h1>03</h1>
+        <h2>Visualize your future</h2>
+        <p>
+          With Smarter Graduate you can compare career paths, the costs of
+          college degrees, and what the lifelong impact is to you. We'll help
+          you visualize your costs, income potential, and student loan impact
+          before making such a big decision. <br />
+          <br />
+          We appreciate you for using Smarter Graduate, and we hope you feel
+          more empowered about your career choices after viewing your analysis.
+        </p>
+        <a href="#" class="more-info-btn">View More</a>
+      </div>
+      <div class="future-image">
+        <img src="images/future.png" alt="vizualize the future" />
+      </div>
+    </div>
+    <!-- Future Section -->
+
+    <!-- Design Section -->
+
+    <div class="design-grid">
+      <div class="design-image">
+        <h1>A Simple Design.</h1>
+        <img src="images/design.png" alt="simple design application" />
+        <h2>
+          The Smarter Graduate analysis tool couldn't be easier to use. See
+          detailed analysis of your financial future with and without expensive
+          college degrees, compare multiple scenarios, and take control of your
+          financial future.
+        </h2>
+        <img src="images/fields.png" alt="simple design application" />
+      </div>
+    </div>
+
+    <!-- Design Section -->
+
+    <!-- Footer Integration -->
+
+    <div class="footer">
+      <div class="footer-copyright">
+        <h1>Copyright &copy; 2019 Lets Talk Develop. All rights reserved.</h1>
+      </div>
+    </div>
+
+    <!-- Footer Integration -->
+
+    <!-- Javascript Integration -->
+
+    <script src="scrolling.js"></script>
+
+    <!-- Javascript Integration -->
+  </body>
+</html>
